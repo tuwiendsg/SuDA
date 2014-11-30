feec030d720e69013192a2c16eb0f392a060bf96
aninditasarkar <sarkar.anindita5@gmail.com>
2014-11-30 14:38:55 +0100

    updating of the java file

--- ProcessModel/src/main/java/at/ac/tuwien/dsg/servicedemand/ResultQuality.java
+++ ProcessModel/src/main/java/at/ac/tuwien/dsg/servicedemand/ResultQuality.java
@@ -5,18 +5,16 @@
  */
 package at.ac.tuwien.dsg.servicedemand;
 
-//import static com.hp.hpl.jena.assembler.JA.Model;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
-//import com.hp.hpl.jena.vocabulary.RDFS;
 import java.io.File;
 import java.io.FileWriter;
 
 /**
  *
- * @author dsg
+ * @author Anindita
  */
 public class ResultQuality {
     
@@ -27,18 +25,14 @@ public class ResultQuality {
     
       String uri="http://DataAnalyticService.com#";
       String objectName="DataAnalyticServiceDemand";
-      
       Model model=ModelFactory.createDefaultModel();
-      //Resource moduleResource=model.createResource(uri+"SensoryModule");
+      
+      //for the rdf file creation
+      {
       Resource demandResource=model.createResource(uri+objectName);
-      //sensorResource.addProperty(RDFS.member, moduleResource);
       
       Property analyticTime=model.createProperty(uri+"analyticTime");
       demandResource.addProperty(analyticTime, "60sec/MB");
-      
-      //Resource gasSensor=model.createResource("http://www.eoc-inc.com/Cambridge/metal%20oxide%20gas%20sensors.htm");
-      //sensorResource.addProperty(RDFS.seeAlso, gasSensor);
-      
       
       Property serviceCost=model.createProperty(uri+"serviceCost");
       demandResource.addProperty(serviceCost,"5 euro/MB");
@@ -54,23 +48,12 @@ public class ResultQuality {
       Property hasQuality=model.createProperty(uri+"hasQuality");
       demandResource.addProperty(hasQuality, qualityResource);
       
-      /////
-      //Property monitoredgasName=model.createProperty(uri+"monitoredgasname");
-     // Property monitoredgasconc=model.createProperty(uri+"monitoredgasconcentration");
-     // sensorResource.addProperty(monitoredgasName, "Methane(CH4)");
-      //sensorResource.addProperty(monitoredgasconc, "1000ppm");
-             
-      //Property position=model.createProperty(uri+"position");
-      //Property board=model.createProperty(uri+"board");
-      //Property sile=model.createProperty(uri+"silo");
-      //Resource location=model.createResource(uri+"location"+objectName);
-      //sensorResource.addProperty(position, "P3");
-      //sensorResource.addProperty(board, "B1");
-      //sensorResource.addProperty(sile, "S5");
-      //sensorResource.addProperty(locationProperty, location);
-      
       model.setNsPrefix("result", uri);
-        model.write(System.out);
+      model.write(System.out);
+    }
+      
+      //for rdf file storage
+      {   
         File f=new File("./example");
             if(!f.exists())
             {
@@ -86,6 +69,9 @@ public class ResultQuality {
             {
                 System.out.println("exception occured : "+e);
             }
+      
+    }
+      
       
       
     }
