1db40ac3e8d842899a69819554ca94640b8376aa
aninditasarkar <sarkar.anindita5@gmail.com>
2015-02-05 17:41:24 +0100

    deletion of user defined process project

---
+++ processmodel/utils/ServiceDemandModel/src/main/java/at/ac/tuwien/dsg/process/AscertainOfProcessModel.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.ac.tuwien.dsg.process;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+import java.io.File;
+import java.io.FileWriter;
+
+/**
+ *
+ * @author dsg
+ */
+public class AscertainOfProcessModel {
+    private static String uri="http://processsequence.com#";
+    private String objectName="ProcessControlFlow";
+    
+    
+    public static void main(String []p)
+    {
+        new AscertainOfProcessModel().modelCreation();
+    }
+    public void modelCreation()
+    {
+        //for assigning of main resource
+      Model model=ModelFactory.createDefaultModel();
+      Resource processResource=model.createResource(uri+objectName);
+      
+        ////Assign the data property of the first process node
+      {
+        Resource processTypeResource=model.createResource("http://DataAnalyticProcessEngine.com#Sampling");
+        Property processTypeProperty=model.createProperty(uri+"hasObject");
+        processResource.addProperty(processTypeProperty, processTypeResource);
+        
+        Property hasStartNodeProperty=model.createProperty(uri+"hasStartNode");
+        processTypeResource.addProperty(hasStartNodeProperty, "Start");
+        
+        Property hasEndNodeProperty=model.createProperty(uri+"hasEndNode");
+        processTypeResource.addProperty(hasEndNodeProperty, "http://DataAnalyticProcessEngine.com#Querying");
+        
+        Property hasResultQualityProperty=model.createProperty(uri+"hasQualityOfResultModel");
+        processTypeResource.addProperty(hasResultQualityProperty, "http://DataAnalyticServiceDemand.com#ResultQualityOfSampling");
+    }
+        ////Assign the data property of the second process node
+        
+        {
+        Resource processTypeResource=model.createResource("http://DataAnalyticProcessEngine.com#Querying");
+        Property processTypeProperty=model.createProperty(uri+"hasObject");
+        processResource.addProperty(processTypeProperty, processTypeResource);
+        
+        Property hasStartNodeProperty=model.createProperty(uri+"hasStartNode");
+        processTypeResource.addProperty(hasStartNodeProperty, "http://DataAnalyticProcessEngine.com#Sampling");
+        
+        Property hasEndNodeProperty=model.createProperty(uri+"hasEndNode");
+        processTypeResource.addProperty(hasEndNodeProperty, "End");
+        
+        Property hasResultQualityProperty=model.createProperty(uri+"hasQualityOfResultModel");
+        processTypeResource.addProperty(hasResultQualityProperty, "http://DataAnalyticServiceDemand.com#ResultQualityOfQuerying");
+    }
+        
+       
+      
+      //for rdf model
+      model.setNsPrefix("process", uri);
+      model.write(System.out);
+      
+      //for rdf file storage
+      {   
+        File f=new File("./ProcessAscertain");
+            if(!f.exists())
+            {
+               f.mkdir();
+               System.out.println("file created");
+            }
+          try
+            {
+                FileWriter fw=new FileWriter("./ProcessAscertain/"+objectName+".rdf");
+                model.write(fw);
+            }
+          catch(Exception e)
+            {
+                System.out.println("exception occured : "+e);
+            }
+      
+    }
+      
+      
+      
+    }
+    }
+
